e7361d35ef45a3b5564bb64fb210afbba16f114c
brian210996 <84860438+brian210996@users.noreply.github.com>
2021-05-28 23:42:54 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,17 +16,47 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click  /predict to know about i and v`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+
+
+state = 0;
+bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
+        `input nilai i|v example 4|8`
     );   
+    state = 1;
 });
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah ${jres[0]} volt`
+            );   
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
+    );   
+})
+    }else{
+        state = 0
+    }
+})
+
+
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    

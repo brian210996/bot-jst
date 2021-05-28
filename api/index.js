c105e7ea3d6fa4a60ef92f66076d79a9176fc1c4
brian210996 <84860438+brian210996@users.noreply.github.com>
2021-05-28 23:04:45 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,16 +28,38 @@ bot.onText(/\/menu/, (msg) => {
     );   
 });
 
-// routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
-    model.predict(
-        [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
-        ]
-    ).then((jres)=>{
-        res.json(jres);
-    })
+state = 0
+bot.onText(/\/predict/, (msg) => { 
+    console.log(msg)
+    bot.sendMessage(
+        msg.chat.id,
+        `masukan nilai i|v contohnya 9|9`
+    );
+    state = 1
 });
 
-module.exports = r;
+bot.on('message', (msg) => {
+    if(state == 1){
+        console.log(msg.text);
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+        [
+            parseFloat(s[0]), // string to float
+            parseFloat(s[1])
+        ]
+    ).then((jres)=>{
+        console.log(jres);
+        bot.sendMessage(
+           msg.chat.id,
+           `nilai v yang diprediksi adalah ${jres[0]} volt`
+    ); 
+        bot.sendMessage(
+           msg.chat.id,
+           `nilai p yang diprediksi adalah ${jres[1]} watt`
+    );
+    })
+    }else{
+        state = 0
+});
